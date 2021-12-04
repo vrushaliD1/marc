2e673da283ea1c9d4e650b122c0b489704cf9bc8
safari-creator <evansmusafa@gmail.com>
2021-12-04 23:18:26 +0530

    'update'

--- src/components/question/Question.js
+++ src/components/question/Question.js
@@ -1,64 +1,44 @@
 import React, { useEffect, useState } from "react";
 import { connect } from "react-redux";
 import { Link, useHistory } from "react-router-dom";
-import Select, {createFilter} from "react-select";
+import Select, { createFilter } from "react-select";
 import { Button, Container, Form, Header, Segment } from "semantic-ui-react";
 import { SAVE_ANSWERS } from "../../store/chapter/constants";
 import { saveUserResponse } from "../../store/chapter/reducer";
 import { abi, tokenContractAddress, client_token, tokens } from "../Data";
-import './Questions.css';
-import Web3 from 'web3';
-
+import "./Questions.css";
+import Web3 from "web3";
 
 function Question(props) {
-  const { chapter,options, saveUserResponse, global, submitAnswer, chapters } = props;
+  const { chapter, options, saveUserResponse, global, submitAnswer, chapters } =
+    props;
   const { questions = [], chapterId } = chapter;
-  const history = useHistory()
+  const history = useHistory();
   const { user } = global;
   const [disabled, setDisabled] = useState(true);
   // const currentChapter = chapters.find((ch)=>ch.chapter_id===chapter.chapter);
-  const [curChapter, setCurChapter] = useState(chapters.find((ch) => ch.chapter._id === chapter.chapter))
-  const [token, setToken] = useState('')
-  const [owned, setOwned] = useState('')
+  const [curChapter, setCurChapter] = useState(
+    chapters.find((ch) => ch.chapter._id === chapter.chapter)
+  );
+  const [token, setToken] = useState("");
+  const [owned, setOwned] = useState("");
 
-  const opt = [
-    { value: "Superman", label: "0" },
-    { value: "5883", label: "1" },
-    { value: "Donkey Kong", label: "2" },
-    { value: "3", label: "3" },
-    { value: "4502", label: "4" },
-    { value: "451", label: "5" },
-    { value: "Steve Rogers", label: "6" },
-    { value: "Albert Einstein", label: "7" },
-    { value: "Satanic leaf-tailed gecko", label: "8" },
-    { value: "plant", label: "9" },
-    { value: "Starman", label: "10" },
-    { value: "Deadheads", label: "11" },
-    { value: "Deadpool", label: "12" },
-    { value: "Kirby", label: "13" },
-    { value: "12", label: "14" },
-    { value: "First to the Egg", label: "15" },
-    { value: "CoinMarketCap", label: "16" },
-    { value: "3112", label: "17" },
-    { value: "Maku", label: "18" },
-    { value: "30", label: "19" },
-  ];
   useEffect(() => {
     if (curChapter) {
-      setToken(curChapter.chapter.token)
-      setOwned(curChapter.owned)
+      setToken(curChapter.chapter.token);
+      setOwned(curChapter.owned);
     }
-  }, [curChapter])
+  }, [curChapter]);
   const { userAddress } = user;
   const handleChange = (e, q) => {
     const { question } = q;
     saveUserResponse({ questionId: question._id, value: e });
-  }
+  };
 
   useEffect(() => {
-    let t = questions.every((q) => q.userResponse !== null)
+    let t = questions.every((q) => q.userResponse !== null);
     setDisabled(!t);
-  }, [questions])
+  }, [questions]);
 
   const handleSubmit = async () => {
     // const web3 = new Web3(Web3.givenProvider);
@@ -69,7 +49,7 @@ function Question(props) {
     }
     submitAnswer({ user, chapter });
     history.push("/answers");
-  }
+  };
   return (
     <Container fluid>
       {
@@ -80,7 +60,7 @@ function Question(props) {
               <Select
                 onChange={(e) => handleChange(e, q)}
                 isSearchable
-                options={opt}
+                options={options}
                 cacheOptions
                 filterOption={createFilter({ ignoreAccents: false })}
               />
@@ -108,15 +88,14 @@ const mapStateToProps = (state) => {
     chapter: state.chapter,
     global: state.global,
     chapters: state.chapters,
-    options: state.options
+    options: state.options,
   };
-}
+};
 
 const mapDispatchToProps = (dispatch) => {
   return {
     saveUserResponse: (data) => dispatch(saveUserResponse(data)),
-    submitAnswer: (data) => dispatch({ type: SAVE_ANSWERS, payload: data })
-    
-  }
-}
+    submitAnswer: (data) => dispatch({ type: SAVE_ANSWERS, payload: data }),
+  };
+};
 export default connect(mapStateToProps, mapDispatchToProps)(Question);
