7edc325055665ba73760d66fd347d4dbeb6fab94
safari-creator <evansmusafa@gmail.com>
2021-12-04 23:05:06 +0530

    update

--- src/api/constants.js
+++ src/api/constants.js
@@ -1,2 +1,2 @@
-export const root_url="http://localhost:8080/api";
-// export const root_url = "http://3.144.217.160:3000/api"
+// export const root_url="http://localhost:8080/api";
+export const root_url = "http://3.144.217.160:3000/api"
